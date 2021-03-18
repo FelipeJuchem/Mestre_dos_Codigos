8d14d387df29ae2aff26ac0deec9df4af9607ae2
Felipe <felipe_build@hotmail.com>
2021-03-18 11:59:39 -0300

    POO

---
+++ UtilizandoPOO/Exercicio1/POO/Carro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POO
+{
+    public class Carro
+    {
+        public string Modelo { get; set; }
+        public string Cor { get; set; }
+        public string Ano { get; set; }
+        public bool Novo { get; set; }
+    }
+}
---
+++ UtilizandoPOO/Exercicio1/POO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace POO
+{
+
+    //Utilizando POO
+    //  Oque é POO?
+    //      POO é paradigma orientado a objetos onde diversas classes tem caracteristicas para definir um objeto na vida real, 
+    //este paradigma se baseia principalmente em dois conceitos: classes e objetos.
+    // Como exemplo eu criei uma classe Carro e criei o objeto carro na classe Program onde agora posso definir os valores do objeto criado.
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro carro = new Carro();
+            carro.Modelo = "Golf";
+            carro.Cor = "Prata";
+            carro.Ano = "2017";
+            carro.Novo = true;
+        }
+    }
+}
